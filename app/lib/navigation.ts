8077f87ec3473568ce77ed077fa37aa4ae255f3e
GauthierWebDev <gauthier@gauthierdaniels.fr>
2025-04-11 16:41:47 +0200

    docs: Remove outdated installation and string theory pages

--- app/lib/navigation.ts
+++ app/lib/navigation.ts
@@ -1,12 +1,5 @@
 export const navigation = [
   {
-    title: "Introduction",
-    links: [
-      { title: "Getting started", href: "/" },
-      { title: "Installation", href: "/docs/installation" },
-    ],
-  },
-  {
     title: "React",
     links: [{ title: "Introduction", href: "/docs/react" }],
   },
