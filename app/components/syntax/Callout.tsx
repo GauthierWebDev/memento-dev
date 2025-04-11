72f0f5731e9a63a10cfadb07005765144fe9435a
GauthierWebDev <gauthier@gauthierdaniels.fr>
2025-04-11 15:40:04 +0200

    style: Add new styles for question component

--- app/components/syntax/Callout.tsx
+++ app/components/syntax/Callout.tsx
@@ -12,6 +12,11 @@ const styles = {
     title: "text-amber-900 dark:text-amber-500",
     body: "text-slate-800 [--tw-prose-underline:var(--color-slate-400)] [--tw-prose-background:var(--color-slate-50)] prose-a:text-slate-900 prose-code:text-slate-900 dark:text-slate-300 dark:[--tw-prose-underline:var(--color-slate-700)] dark:prose-code:text-slate-300",
   },
+  question: {
+    container: "bg-amber-50 dark:bg-amber-800/60 dark:ring-1 dark:ring-amber-300/10",
+    title: "text-amber-900 dark:text-amber-500",
+    body: "text-slate-800 [--tw-prose-underline:var(--color-slate-400)] [--tw-prose-background:var(--color-slate-50)] prose-a:text-slate-900 prose-code:text-slate-900 dark:text-slate-300 dark:[--tw-prose-underline:var(--color-slate-700)] dark:prose-code:text-slate-300",
+  },
 };
 
 const icons = {
