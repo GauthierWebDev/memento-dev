7828846362a3a570494c421df4677700661a2c4b
GauthierWebDev <gauthier@gauthierdaniels.fr>
2025-04-11 11:46:22 +0200

    refactor: Update import statements in Search.telefunc.ts

--- app/components/syntax/Search.telefunc.ts
+++ app/components/syntax/Search.telefunc.ts
@@ -1,14 +1,7 @@
-// import type { SearchResult } from "@/lib/search";
-import type { SearchResult } from "@/services/FlexSearchService";
-
-// import { buildSearchIndex, search } from "@/lib/search";
-import { buildFlexSearch } from "@/services/FlexSearchService";
+import { buildFlexSearch, type SearchResult } from "@/services/FlexSearchService";
 import { docsService } from "@/services/DocsService";
 
 export const onSearch = async (query: string): Promise<SearchResult[]> => {
-  // const searchIndex = buildSearchIndex("./data/docs");
-  // return search(searchIndex, query);
-
   const search = buildFlexSearch(await docsService.fetchDocs());
   const results = search(query);
 
