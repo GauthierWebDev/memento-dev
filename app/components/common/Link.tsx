67f2ff697ebf63ab8f9c9d52acc13d705c6215c2
GauthierWebDev <gauthier@gauthierdaniels.fr>
2025-04-17 20:04:47 +0200

    feat: Add prefetching for links to improve performance

--- app/components/common/Link.tsx
+++ app/components/common/Link.tsx
@@ -1,4 +1,5 @@
 import { usePageContext } from "vike-react/usePageContext";
+import { prefetch } from "vike/client/router";
 import clsx from "clsx";
 
 export function Link(props: React.AnchorHTMLAttributes<HTMLAnchorElement> & { href: string }) {
@@ -7,6 +8,8 @@ export function Link(props: React.AnchorHTMLAttributes<HTMLAnchorElement> & { hr
   const isSameDomain = !(props.href.startsWith("http") || props.href.startsWith("mailto"));
   const isDownload = props.href.endsWith(".pdf") || props.href.endsWith(".zip");
 
+  const handleMouseEnter = () => prefetch(props.href);
+
   return (
     <a
       {...props}
@@ -14,6 +17,7 @@ export function Link(props: React.AnchorHTMLAttributes<HTMLAnchorElement> & { hr
       className={clsx(isActive && "is-active", props.className)}
       {...(isDownload ? { download: true } : {})}
       {...(!isSameDomain || isDownload ? { target: "_blank", rel: "noopener noreferrer" } : {})}
+      {...(isSameDomain ? { onMouseEnter: handleMouseEnter } : {})}
     >
       {props.children}
     </a>
