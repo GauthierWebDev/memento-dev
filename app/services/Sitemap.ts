3a84a78c435b1851f909df36a44c66ee7c424ce8
GauthierWebDev <gauthier@gauthierdaniels.fr>
2025-04-18 17:43:25 +0200

    feat: Add new script for generating sitemap

--- app/services/Sitemap.ts
+++ app/services/Sitemap.ts
@@ -19,6 +19,8 @@ type SitemapElement = {
 };
 
 class Sitemap {
+  private readonly pagesPath = path.join(__dirname, "pages");
+  private readonly dataPath = path.join(__dirname, "data");
   private readonly sitemapPath = path.join(__dirname, "public", "sitemap.xml");
   private readonly lastModified = new Date().toISOString();
   private readonly baseUrl = getBaseUrl();
@@ -51,8 +53,11 @@ class Sitemap {
     this.sitemap += `</urlset>`;
   }
 
+  private addSitemapElement(url: SitemapElement): void {}
+
   private buildSitemap(): void {
     this.prependSitemap();
+    this.urls.forEach(this.addSitemapElement.bind(this));
     this.appendSitemap();
   }
 
@@ -60,7 +65,40 @@ class Sitemap {
     fs.writeFileSync(this.sitemapPath, this.sitemap, "utf8");
   }
 
-  private loadUrls(): void {}
+  private loadPriority(href: string): string {
+    const isRootUrl = ["/", ""].includes(href);
+    const isMainUrl = ["/docs", "/certifications", "/politique-de-confidentialite", "/mentions-legales"].includes(href);
+
+    if (isRootUrl) return "1.0";
+    if (isMainUrl) return "0.9";
+    return "0.8";
+  }
+
+  private loadLastModified(href: string): string {}
+
+  private loadFile(href: string) {}
+
+  private loadUrls(): void {
+    this.urls = navigation.flatMap((item) => {
+      return item.links
+        .map((link) => {
+          const file = this.loadFile(link.href);
+          if (!file) {
+            console.warn(`File not found for URL: ${link.href}`);
+            return null;
+          }
+
+          return {
+            location: `${this.baseUrl}${link.href}`,
+            lastmod: this.loadLastModified(link.href),
+            priority: this.loadPriority(link.href),
+          };
+        })
+        .filter((url) => url !== null);
+    });
+
+    console.log("Loaded URLs:", this.urls);
+  }
 
   public generateSitemap(): void {
     console.log("Generating sitemap...");
