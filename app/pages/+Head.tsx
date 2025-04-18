b8e58ce301491befd8b4b8663bcdf53bfe5ed6ad
GauthierWebDev <gauthier@gauthierdaniels.fr>
2025-04-18 13:08:45 +0200

    feat: Add tracking scripts for Umami and Google Tag Manager

--- app/pages/+Head.tsx
+++ app/pages/+Head.tsx
@@ -5,6 +5,17 @@ export default function HeadDefault() {
   return (
     <>
       <link rel="icon" href={logoUrl} />
+      <script defer src="https://cloud.umami.is/script.js" data-website-id="ba70261e-d145-4dd1-b0e8-27cbf4927b74" />
+
+      <script async src={`https://www.googletagmanager.com/gtag/js?id=GTM-NRMKQ68K`}></script>
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'GTM-NRMKQ68K');`,
+        }}
+      ></script>
     </>
   );
 }
