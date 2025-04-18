9ebf7f1cf79aeb41084a1c7558819ff8c5f61156
GauthierWebDev <gauthier@gauthierdaniels.fr>
2025-04-18 12:08:50 +0200

    docs: Add workaround comment for Markdoc export issue

--- app/markdoc/Tag.ts
+++ app/markdoc/Tag.ts
@@ -5,6 +5,9 @@ import Markdoc from "@markdoc/markdoc";
 
 type TagAttributesValue = string | number | boolean | null | undefined | object | unknown;
 
+// Workaround for Markdoc, that's not exporting the type
+// in production build (Vite issue).
+// See: https://stackoverflow.com/questions/76191154/the-requested-module-does-not-provide-an-export-named-default
 const { Tag: MarkdocTag } = Markdoc;
 
 export class Tag extends MarkdocTag {
