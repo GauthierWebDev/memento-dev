2e7c4fff6e3a145f3b275164ba92798ddbae9e02
GauthierWebDev <gauthier@gauthierdaniels.fr>
2025-04-18 21:06:58 +0200

    style: Fix missing closing tag in Sitemap.ts

--- app/services/Sitemap.ts
+++ app/services/Sitemap.ts
@@ -46,7 +46,7 @@ class Sitemap {
 
   private prependSitemap(): void {
     this.sitemap = `<?xml version="1.0" encoding="UTF-8"?>`;
-    this.sitemap += `<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"`;
+    this.sitemap += `<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">`;
   }
 
   private appendSitemap(): void {
