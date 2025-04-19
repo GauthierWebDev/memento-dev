67140bb47ec013789a2be5cd0506367782a8208c
GauthierWebDev <gauthier@gauthierdaniels.fr>
2025-04-19 13:45:33 +0200

    feat: Add production script to package.json

--- app/config.ts
+++ app/config.ts
@@ -6,8 +6,16 @@ function getEnvironmentVariable<T = undefined>(key: string, defaultValue: T, for
   return value as T;
 }
 
-const PORT = getEnvironmentVariable<number>('PORT', 3000, (data) => parseInt(data, 10));
-const HMR_PORT = getEnvironmentVariable<number>('HMR_PORT', PORT + 1, (data) => parseInt(data, 10));
+function getEnvironmentVariableOrThrow<T = undefined>(key: string, formatter?: (data: string) => T): T {
+  const value = process.env[key];
+  
+  if (value === undefined) throw new Error(`Missing environment variable: ${key}`);
+  if (formatter) return formatter(value);
+  return value as T;
+}
+
+const PORT = getEnvironmentVariableOrThrow<number>('PORT', (data) => parseInt(data, 10));
+const HMR_PORT = getEnvironmentVariableOrThrow<number>('HMR_PORT', (data) => parseInt(data, 10));
 const BASE_URL = getEnvironmentVariable<string>('BASE_URL', `http://localhost:${PORT}`);
 const NODE_ENV = getEnvironmentVariable<string>('NODE_ENV', 'development');
 
--- app/fastify-entry.ts
+++ app/fastify-entry.ts
@@ -1,67 +1,64 @@
-import { dirname } from "node:path";
 import { fileURLToPath } from "node:url";
+import { dirname } from "node:path";
 
-import { vikeHandler } from "./server/vike-handler";
-import { telefuncHandler } from "./server/telefunc-handler";
-import Fastify from "fastify";
 import { createHandler } from "@universal-middleware/fastify";
+import { telefuncHandler } from "./server/telefunc-handler";
+import { vikeHandler } from "./server/vike-handler";
+import { config } from "./config";
+import Fastify from "fastify";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 const root = __dirname;
-const port = process.env.PORT ? parseInt(process.env.PORT, 10) : 3000;
-const hmrPort = process.env.HMR_PORT ? parseInt(process.env.HMR_PORT, 10) : 24678;
 
 async function startServer() {
-  const app = Fastify();
+	const app = Fastify();
 
-  // Avoid pre-parsing body, otherwise it will cause issue with universal handlers
-  // This will probably change in the future though, you can follow https://github.com/magne4000/universal-middleware for updates
-  app.removeAllContentTypeParsers();
-  app.addContentTypeParser("*", function (_request, _payload, done) {
-    done(null, "");
-  });
+	// Avoid pre-parsing body, otherwise it will cause issue with universal handlers
+	// This will probably change in the future though, you can follow https://github.com/magne4000/universal-middleware for updates
+	app.removeAllContentTypeParsers();
+	app.addContentTypeParser("*", (_request, _payload, done) => done(null, ""));
 
-  await app.register(await import("@fastify/middie"));
+	await app.register(await import("@fastify/middie"));
 
-  if (process.env.NODE_ENV === "production") {
-    await app.register(await import("@fastify/static"), {
-      root: `${root}/dist/client`,
-      wildcard: false,
-    });
-  } else {
-    // Instantiate Vite's development server and integrate its middleware to our server.
-    // ⚠️ We should instantiate it *only* in development. (It isn't needed in production
-    // and would unnecessarily bloat our server in production.)
-    const vite = await import("vite");
-    const viteDevMiddleware = (
-      await vite.createServer({
-        root,
-        server: { middlewareMode: true, hmr: { port: hmrPort } },
-      })
-    ).middlewares;
-    app.use(viteDevMiddleware);
-  }
+	if (process.env.NODE_ENV === "production") {
+		await app.register(await import("@fastify/static"), {
+			root: `${root}/dist/client`,
+			wildcard: false,
+		});
+	} else {
+		// Instantiate Vite's development server and integrate its middleware to our server.
+		// ⚠️ We should instantiate it *only* in development. (It isn't needed in production
+		// and would unnecessarily bloat our server in production.)
+		const vite = await import("vite");
+		const viteDevMiddleware = (
+			await vite.createServer({
+				root,
+				server: { middlewareMode: true, hmr: { port: config.HMR_PORT } },
+			})
+		).middlewares;
+		app.use(viteDevMiddleware);
+	}
 
-  app.post<{ Body: string }>("/_telefunc", createHandler(telefuncHandler)());
+	app.post<{ Body: string }>("/_telefunc", createHandler(telefuncHandler)());
 
-  /**
-   * Vike route
-   *
-   * @link {@see https://vike.dev}
-   **/
-  app.all("/*", createHandler(vikeHandler)());
+	/**
+	 * Vike route
+	 *
+	 * @link {@see https://vike.dev}
+	 **/
+	app.all("/*", createHandler(vikeHandler)());
 
-  return app;
+	return app;
 }
 
 const app = await startServer();
 
-app.listen(
-  {
-    port: port,
-  },
-  () => {
-    console.log(`Server listening on http://localhost:${port}`);
-  },
-);
+app.listen({ port: config.PORT, host: "0.0.0.0" }, (error, address) => {
+	if (error) {
+		app.log.error(error);
+		process.exit(1);
+	}
+
+	console.log(`Server listening on ${address}`);
+});
