9e27c0bef3b291214bdac3efbcc7dce6d73556ae
GauthierWebDev <gauthier@gauthierdaniels.fr>
2025-04-20 13:06:36 +0200

    style: improve code readability and structure

--- app/pages/+data.ts
+++ app/pages/+data.ts
@@ -1,9 +1,27 @@
 import type { PageContext } from "vike/types";
 
+import { useConfig } from "vike-solid/useConfig";
+import buildTitle from "./buildTitle";
+
 export type Data = Awaited<ReturnType<typeof data>>;
 
 export async function data(pageContext: PageContext) {
+	const config = useConfig();
+
+	const {
+		exports: { tableOfContents, frontmatter },
+		urlParsed,
+	} = pageContext;
+	const isRoot = urlParsed.pathname === "/";
+
+	console.log({ isRoot, urlParsed });
+
+	config({
+		title: buildTitle(isRoot ? undefined : frontmatter?.title),
+		description: frontmatter?.description,
+	});
+
 	return {
-		tableOfContents: pageContext.exports.tableOfContents,
+		tableOfContents,
 	};
 }
