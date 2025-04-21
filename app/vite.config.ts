aeb6f5da5a25c7f69efd441fc7c71228122e5781
GauthierWebDev <gauthier@gauthierdaniels.fr>
2025-04-21 21:42:31 +0200

    style: Add tailwindcss class for background colors

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -40,7 +40,6 @@ export default defineConfig({
 		vikeSolid(),
 		mdx({
 			jsxImportSource: "solid-jsx",
-			// providerImportSource: "solid-mdx",
 			remarkPlugins: [
 				remarkFrontmatter,
 				remarkHeadingId,
