4cab7bb6eda337757f5f846d4c5e3b1e4fbc0490
GauthierWebDev <gauthier@gauthierdaniels.fr>
2025-04-21 14:42:35 +0200

    feat: Add NProgress for page transitions

--- app/pages/+onPageTransitionStart.ts
+++ app/pages/+onPageTransitionStart.ts
@@ -1,6 +1,7 @@
 import type { OnPageTransitionStartAsync } from "vike/types";
 
+import NProgress from "nprogress";
+
 export const onPageTransitionStart: OnPageTransitionStartAsync = async () => {
-  console.log("Page transition start");
-  document.querySelector("body")?.classList.add("page-is-transitioning");
+	NProgress.start();
 };
